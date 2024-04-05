5500d6fbdcd8d581ccc6206d9a7454d1445a11ca
Akshay Kumar <143085291+SiriusOp@users.noreply.github.com>
2024-04-05 18:08:30 +0000

    strings

---
+++ 01_Basics/07_strings.js
@@ -0,0 +1,43 @@
+const name = "Akshay"
+const repocount = 50
+
+// console.log(name + repocount +"Value"); // Old method 
+
+// console.log(`Hello my name is ${name} and my repocount is ${repocount}.`);   // string interpolation
+
+// Another method of string declaration
+
+// const gamename = new String('Akshay')  // Object
+
+// console.log(gamename[0]);
+// console.log(gamename.__proto__);
+
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf('s'));
+
+// const newstrng = gamename.substring(0,4);
+// console.log(newstrng);
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+// const stringone = "    Akshay    "
+// console.log(stringone);
+// console.log(stringone.trim());    // remove spaces from start and end
+
+// const url = "https://akshay.com/akshay%20kumar";
+
+// console.log(url.replace("%20",'-'));
+// console.log(url.includes("akshay"));
+
+
+
+// Now Convert string into array 
+
+const StringName = "Akshay-Kumar-MCA"
+
+const array = StringName.split('-')
+
+console.log(array);
