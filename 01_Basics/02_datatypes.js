b5a586fa7d605b13dcbb764bc0ad85d3a9e96eb5
Akshay Kumar <143085291+SiriusOp@users.noreply.github.com>
2024-04-05 06:21:33 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict" // treat all js code as newer version 
+
+// alert(3+3) // we are using node.js, not browser
+
+console.log(3
+    +
+    3)    // code readibility should be high
+
+console.log("Akshay");
+
+
+
+// Data types
+
+let name = "Akshay"   // string
+let age = 18          // int   range => 2 to the power 53, bigger number we use bigint
+let isStudent = true  // boolean
+
+// int
+// bigint
+// string
+// boolean
+// null  => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+
+// check type of the data
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof null);  // object
+console.log(typeof undefined);   // undefined
