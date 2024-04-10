7240a69a0ef08d3184e1a1f8c63e82acbeed8889
Akshay Kumar <143085291+SiriusOp@users.noreply.github.com>
2024-04-10 08:13:20 +0000

    control flow

---
+++ 03_truthy.js
@@ -0,0 +1,23 @@
+const useremail = "akshaykumar.abesec@gmail.com";
+
+if(useremail){
+    console.log("Got an email");
+}else{
+    console.log("Email not found");
+}
+
+
+
+// falsy values
+// 0, false, -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+
+
+
+// const arr = [];
+// if(arr.length === 0){
+//     console.log("Array is Empty");
+// }
