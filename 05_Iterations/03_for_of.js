c81a5a00466a2dab681e250a6360a1a366a04d38
Akshay Kumar <143085291+SiriusOp@users.noreply.github.com>
2024-04-10 14:02:32 +0000

    For of

---
+++ 05_Iterations/03_for_of.js
@@ -0,0 +1,49 @@
+// forof loop
+
+// syntax
+// for (const iterator of object) {   
+// }
+
+
+// ["", "", ""]
+// [{},{},{}]
+
+
+
+// const arr = [1,2,3,4,5]
+
+// for(const value of arr){
+//     console.log(value);
+// }
+
+
+
+// const greetings = "Hello World!"
+
+// for(const greet of greetings){
+//     console.log(greet);
+// }
+
+
+
+// Map - store the value in key - value pair, but duplicacy not allowed
+
+// const info = new Map();
+// info.set('IN',"India")
+// info.set('USA',"United States of America")
+// info.set('Fr',"France")
+// info.set('IN',"India")
+
+// console.log(info);
+
+// for(const key of info){
+//    console.log(key);
+// }
+
+// for(const [key] of info){
+//     console.log(key);
+//  }
+
+//  for(const [key,value] of info){
+//     console.log(key,value);
+//  }
